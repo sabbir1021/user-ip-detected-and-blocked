0a297c649d54b3985d2cce71d35e7dd1c8670e39
sabbir1021 <5boysbd@gmail.com>
2022-07-03 15:17:34 +0600

    validated with block time and count

--- src/ip/views.py
+++ src/ip/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login as dj_login , logout as dj_logout
 from django.contrib import messages
 from .models import RequestIP
-from datetime import datetime
-now = datetime.now()
+from datetime import timedelta
+from django.utils import timezone
 
 # Create your views here.
 
@@ -18,18 +18,29 @@ def login(request):
         user_ip = request.POST.get("ip")
         
         if not RequestIP.objects.filter(ip_address = user_ip).exists():
-            req_ip = RequestIP.objects.create(ip_address = user_ip)
+            req_ip = RequestIP.objects.create(ip_address = user_ip, block_time=timezone.now())
         else:
             req_ip = RequestIP.objects.get(ip_address = user_ip)
         
-        
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            dj_login(request, user)
-            messages.success(request, 'You were successfully login')
-            return redirect('ip:home')
+        if timezone.now() >= req_ip.block_time:
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                dj_login(request, user)
+                req_ip.bad_request_count = 0
+                req_ip.save()
+                messages.success(request, 'You were successfully login')
+                return redirect('ip:home')
+            else:
+                if req_ip.bad_request_count == 2:
+                    req_ip.block_time = timezone.now() + timedelta(hours=1)
+                    req_ip.bad_request_count = 0
+                    req_ip.save()
+                    messages.success(request, 'Your IP is block for 1 hour')
+                else:
+                    req_ip.bad_request_count = int(req_ip.bad_request_count+1)
+                    req_ip.save()
+                    messages.success(request, 'Your username password mistchmatch')
         else:
-            req_ip.bad_request_count = int(req_ip.bad_request_count+1)
-            req_ip.save()
-            messages.success(request, 'Your username password mistchmatch')
+            times = int((req_ip.block_time - timezone.now()).total_seconds()/60)
+            messages.success(request, f'Your IP is block for 1 hour. After {times} min you can access.')
     return render(request, "login.html")
